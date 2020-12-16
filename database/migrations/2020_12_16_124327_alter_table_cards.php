83c768a6cfbe1ce209fb1790f9eea41441d8a05f
Mohamad.mu <mhdite7@gmail.com>
2020-12-16 15:04:16 +0200

    fix 5%

--- database/migrations/2020_12_16_124327_alter_table_cards.php
+++ database/migrations/2020_12_16_124327_alter_table_cards.php
@@ -14,7 +14,7 @@ class AlterTableCards extends Migration
     public function up()
     {
         Schema::table('cards', function (Blueprint $table) {
-            $table->string('name');
+            $table->string('name')->nullable();
         });
     }
 
