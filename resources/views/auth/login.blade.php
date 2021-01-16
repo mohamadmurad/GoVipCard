a3a9a1d8e0f5a141aae589675dc90cd19c17e019
Mohamad.mu <mero22.4.1997>
2021-01-16 17:58:07 +0200

    fix 5%

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 <x-guest-layout>
     <x-jet-authentication-card>
         <x-slot name="logo">
-            <x-jet-authentication-card-logo />
+           <h3>Go Toys Vip Cards</h3>
         </x-slot>
 
         <x-jet-validation-errors class="mb-4" />
