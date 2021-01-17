ea54ebb378b645a4f070eab6d007c4271a2438d7
Mohamad.mu <mero22.4.1997>
2021-01-17 15:02:03 +0200

    fix 5%

--- resources/views/card/create.blade.php
+++ resources/views/card/create.blade.php
@@ -7,7 +7,7 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-2">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-2 p" style="padding: 30px">
 
 
 
