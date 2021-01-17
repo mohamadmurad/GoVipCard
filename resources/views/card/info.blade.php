912db8e2edfc8dd49296e9a8329bc659998b6a9c
Mohamad.mu <mero22.4.1997>
2021-01-17 15:14:20 +0200

    fix 5%

--- resources/views/card/info.blade.php
+++ resources/views/card/info.blade.php
@@ -1,6 +1,9 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 float-right">
+
+
+
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('البطاقة رقم ' .$card->barcode ) }}
         </h2>
     </x-slot>
--- resources/views/card/report.blade.php
+++ resources/views/card/report.blade.php
@@ -1,6 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 float-right">
+
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('معلومات عن الكروت' ) }}
         </h2>
     </x-slot>
