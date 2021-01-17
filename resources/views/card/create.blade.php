c6bcd561e70695736f66c80f9dd76f786dde45ef
Mohamad.mu <mero22.4.1997>
2021-01-17 15:13:02 +0200

    fix 5%

--- resources/views/card/create.blade.php
+++ resources/views/card/create.blade.php
@@ -1,7 +1,8 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 float-right">
-            {{ __('إضافة كروت ' ) }}
+
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('إضافة كروت' ) }}
         </h2>
     </x-slot>
 
